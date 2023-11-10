b822092467007970a94a7e2780cd965aefe3fdf8
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2023-11-10 18:53:07 +0700

    Updagte b8

--- CONVERT/assets/js/custom-owl.js
+++ CONVERT/assets/js/custom-owl.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $(".owl-carousel").owlCarousel();
-  
+  $('.owl-carousel').owlCarousel({
+    loop: true, /* Vong lap slide*/
+    margin: 20, /* K/C giua 2 phan tu*/
+    nav: false, /* Thanh dieu huong*/
+    dots: false, /* Dau cham */
+    autoplay: true, /* Slider tu dong chay*/
+    autoplayTimeout: 2000, /*Thoi gian chuyen slider*/
+    responsive: {
+      /* Hien thi item tren cac khoang man hinh */
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items:1
+      }
+    }
+  })
+
 });
